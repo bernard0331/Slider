7ec7709dc70f8cdf4d941e34b93362554d8ed4a8
bernard0331 <47488073+bernard0331@users.noreply.github.com>
2022-03-17 23:24:19 -0400

    Key Bind HInts
    Hints that display on screen whenever the player doesn't do any action and cycle constantly.

--- Slider/Assets/Scripts/UI/KeyBindHints.cs
+++ Slider/Assets/Scripts/UI/KeyBindHints.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class KeyBindHints : MonoBehaviour
 {
     private InputSettings controls;
     public KeyTimer[] keyTimers;
-
+    public TextMeshProUGUI hintText;
+    public GameObject canvas;
+    public static bool doubleSizeMode = false;
+    public static bool highContrastMode = false;
+    private int currNumHintsDisplayed = 0;
 
     [System.Serializable]
     public class KeyTimer
@@ -22,6 +27,7 @@ public class KeyBindHints : MonoBehaviour
         public float timeUntilHint;
         public string message;
         public bool displayed;
+        public bool enabled;
     }
 
     private void Awake()
@@ -31,6 +37,13 @@ public class KeyBindHints : MonoBehaviour
         controls.Player.CycleEquip.performed += context => CheckControls(KeyTimer.ControlTypes.Q);
         controls.Player.Action.performed += context => CheckControls(KeyTimer.ControlTypes.E);
         controls.UI.OpenArtifact.performed += context => CheckControls(KeyTimer.ControlTypes.Tab);
+    }
+
+    void Start()
+    {
+        hintText.gameObject.SetActive(false);
+        hintText.text = "";
+        canvas.SetActive(false);
     }
 
     private void OnEnable()
@@ -43,10 +56,6 @@ public class KeyBindHints : MonoBehaviour
         controls.Disable();
     }
 
-    void Start()
-    {
-    }
-
     void Update()
     {
         foreach (KeyTimer kt in keyTimers)
@@ -54,18 +63,17 @@ public class KeyBindHints : MonoBehaviour
             if (!kt.displayed)
             {
                 kt.timeUntilHint -= Time.deltaTime;
-                if (kt.timeUntilHint <= 0)
+                if (kt.timeUntilHint <= 0 && !kt.enabled)
                 {
-                    DisplayHint(kt);
+                    //StopAllCoroutines();
+                    currNumHintsDisplayed++;
+                    DisplaySentence(kt);
+                    kt.enabled = true;
                 }
             }
         }
     }
 
-    private void DisplayHint(KeyTimer kt)
-    {
-        Debug.Log(kt.message);
-    }
 
     public void CheckControls(KeyTimer.ControlTypes controlType)
     {
@@ -77,4 +85,54 @@ public class KeyBindHints : MonoBehaviour
             }
         }
     }
+
+    public void DisplaySentence(KeyTimer kt)
+    {
+        string hint = kt.message;
+        CheckSize();
+        canvas.SetActive(true);
+        hintText.gameObject.SetActive(true);
+        //StartCoroutine(TypeSentence(hint));
+        hintText.text += hint + "\n";
+        StartCoroutine(HintWait(kt));
+    }
+
+    IEnumerator HintWait(KeyTimer kt)
+    {
+        yield return new WaitForSeconds(5);
+        RemoveHint(kt);
+    }
+
+    public void RemoveHint(KeyTimer kt)
+    {
+        print(hintText.text);
+        if (currNumHintsDisplayed > 1)
+        {
+            hintText.text = hintText.text.Substring(0, hintText.text.IndexOf(kt.message)) + hintText.text.Substring(hintText.text.IndexOf(kt.message) + kt.message.Length + 1);
+        }
+        else
+        {
+            canvas.SetActive(false);
+            hintText.gameObject.SetActive(false);
+            hintText.text = "";
+        }
+        kt.enabled = false;
+        kt.timeUntilHint = 10;
+        //kt.displayed = false;
+        currNumHintsDisplayed--;
+    }
+
+
+    private void CheckSize()
+    {
+        if (doubleSizeMode)
+        {
+            canvas.transform.localScale = new Vector3(2, 2, 2);
+        }
+        else
+        {
+            canvas.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
 }
+
